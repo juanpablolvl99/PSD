70407aee887352b6363f9599bea80b1b70ebf3dc
juanpablo <juanpablolvl99@gmail.com>
2017-03-12 12:34:30 -0300

    estrutura desfeita

--- projeto/src/projeto/psd/daos/UsuarioDao.java
+++ projeto/src/projeto/psd/daos/UsuarioDao.java
@@ -7,8 +7,6 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import projeto.psd.entidades.Usuario;
 import projeto.psd.interfaces.UsuarioDaoIf;
 
