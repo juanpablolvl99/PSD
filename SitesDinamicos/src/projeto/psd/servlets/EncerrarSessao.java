fc91c9b435819ab4a2cd8d2367151de132d1c354
rodrigobentorodrigues <rodrigobentor2014@gmail.com>
2017-04-12 20:08:15 -0300

    Encerrar Sessão criado

---
+++ SitesDinamicos/src/projeto/psd/servlets/EncerrarSessao.java
@@ -0,0 +1,30 @@
+
+package projeto.psd.servlets;
+
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class EncerrarSessao extends HttpServlet{
+    
+    @Override
+    public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException{
+
+        HttpSession sessao = req.getSession(false);
+        synchronized(sessao){
+            sessao.invalidate();
+            
+        }
+        try {
+            resp.sendRedirect("index.htm");
+        } catch (IOException ex) {
+            resp.getWriter().print(ex);
+        }
+
+    }
+    
+}
